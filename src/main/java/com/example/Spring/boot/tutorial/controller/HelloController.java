0483b1c2e61f7ff346456fa41a5f84c71024091e
narojunagaraju <narojunagarajun@gmail.com>
2024-04-21 12:49:57 +0530

    get mapping

--- src/main/java/com/example/Spring/boot/tutorial/controller/HelloController.java
+++ src/main/java/com/example/Spring/boot/tutorial/controller/HelloController.java
@@ -1,13 +1,12 @@
 package com.example.Spring.boot.tutorial.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String helloWorld() {
         return "Hey, Welcome to Spring boot tutorial";
     }
