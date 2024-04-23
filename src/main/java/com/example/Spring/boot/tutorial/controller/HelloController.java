fee688b2f38657586f99558f75127d47705214e1
narojunagaraju <narojunagarajun@gmail.com>
2024-04-23 13:52:01 +0530

    adding properties in application properties file

--- src/main/java/com/example/Spring/boot/tutorial/controller/HelloController.java
+++ src/main/java/com/example/Spring/boot/tutorial/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.Spring.boot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+
     @GetMapping("/")
     public String helloWorld() {
-        return "Hey, Welcome to Spring boot tutorial";
+        return "Hey, " + welcomeMessage;
     }
 }
